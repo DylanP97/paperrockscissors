697454f619bb863aaeda909f4cab3aed1ec4c0ff
DylanP97 <dylan.pinheiro.1997@gmail.com>
2022-06-16 05:51:06 +0200

    paper rock scissors

--- game.js
+++ game.js
@@ -2,30 +2,36 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+let userChoiceID
 let userChoice
 let computerChoice
 let result
+let timeout
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
-    userChoice = e.target.id
+    userChoiceID = e.target.id
+    userChoice = e.target.innerHTML
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
     getResult()
+    console.log (userChoice)
 }))
+
+
 
 function generateComputerChoice () {
     const randomNumber = Math.floor(Math.random() * 3) + 1
     
     if (randomNumber === 1) {
-        computerChoice = 'Rock'
+        computerChoice = 'Rock <i class="fa-solid fa-hand-back-fist"></i>'
     }
 
     if (randomNumber === 2) {
-        computerChoice = 'Scissors'
+        computerChoice = 'Scissors <i class="fa-solid fa-hand-scissors"></i>'
     }
 
     if (randomNumber === 3) {
-        computerChoice = 'Paper'
+        computerChoice = 'Paper <i class="fa-solid fa-hand"></i>'
     }
 
     computerChoiceDisplay.innerHTML = computerChoice
@@ -36,27 +42,27 @@ function getResult () {
         result = 'its a draw!'
     }
 
-    if (computerChoice === 'Rock' && userChoice === 'Paper') {
+    if (computerChoice === 'Rock <i class="fa-solid fa-hand-back-fist"></i>' && userChoice === 'Paper <i class="fa-solid fa-hand"></i>') {
         result = 'you win!'
     }
 
-    if (computerChoice === 'Rock' && userChoice === 'Scissors') {
+    if (computerChoice === 'Rock <i class="fa-solid fa-hand-back-fist"></i>' && userChoice === 'Scissors <i class="fa-solid fa-hand-scissors"></i>') {
         result = 'you lost!'
     }
 
-    if (computerChoice === 'Paper' && userChoice === 'Scissors') {
+    if (computerChoice === 'Paper <i class="fa-solid fa-hand"></i>' && userChoice === 'Scissors <i class="fa-solid fa-hand-scissors"></i>') {
         result = 'you win!'
     }
 
-    if (computerChoice === 'Paper' && userChoice === 'Rock') {
+    if (computerChoice === 'Paper <i class="fa-solid fa-hand"></i>' && userChoice === 'Rock <i class="fa-solid fa-hand-back-fist"></i>') {
         result = 'you lost!'
     }
 
-    if (computerChoice === 'Scissors' && userChoice === 'Rock') {
+    if (computerChoice === 'Scissors <i class="fa-solid fa-hand-scissors"></i>' && userChoice === 'Rock <i class="fa-solid fa-hand-back-fist"></i>') {
         result = 'you win!'
     }
 
-    if (computerChoice === 'Scissors' && userChoice === 'Paper') {
+    if (computerChoice === 'Scissors <i class="fa-solid fa-hand-scissors"></i>' && userChoice === 'Paper <i class="fa-solid fa-hand"></i>') {
         result = 'you lost!'
     }
 
